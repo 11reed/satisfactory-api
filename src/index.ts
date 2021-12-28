3026af2cae9982685d74dc978c5db6246562e818
Tim Reed <reed11tim@gmail.com>
2021-12-28 01:59:56 -0500

    major changes

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,80 @@
-import { Drash } from "https://deno.land/x/drash@v1.5.1/mod.ts";
-import { Cors } from "https://deno.land/x/drash_middleware@v0.7.9/cors/mod.ts";
+import { bold, yellow } from 'https://deno.land/std@0.118.0/fmt/colors.ts';
+import { Application, Context, Router, isHttpError } from 'https://deno.land/x/oak/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
 
-import all from './routes/all.ts';
-import belts from './routes/belts.ts';
-import buildings from './routes/buildings.ts';
-import items from './routes/items.ts';
-import miners from './routes/miners.ts';
-import resources from './routes/resources.ts';
+import all from './data/allData.ts';
+import belts from './data/beltsData.ts';
+import buildings from './data/buildingsData.ts';
+import items from './data/itemsData.ts';
+import miners from './data/minersData.ts';
+import resources from './data/resourcesData.ts';
 
-const server = new Drash.Http.Server({
-  logger: new Drash.CoreLoggers.ConsoleLogger({
-    enabled: true,
-    level: "debug",
-  }),
-  middleware: {
-    after_request: [
-      Cors(),
-    ],
-  },
-  response_output: "application/json",
-  resources: [all, belts, buildings, items, miners, resources]
+const app = new Application();
+const router = new Router();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(oakCors({origin: '*'}));
+
+app.use(async (ctx: Context, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (isHttpError(err)) {
+      ctx.response.status = err.status;
+      if (ctx.request.accepts('json')) {
+        ctx.response.body = err;
+        ctx.response.type = 'json';
+      } else {
+        console.log(err);
+        throw err;
+      }
+    }
+  }
 });
 
-server.run({
-  hostname: "localhost",
-  port: 8080
+router.use(async (ctx: Context, next) => {
+  await next();
+  const responseTime = ctx.response.headers.get('X-Response-Time');
+  console.log(responseTime);
 });
 
-console.log(`Server running at ${server.hostname}:${server.port}`);
+router.use(async (ctx: Context, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set('X-Response-Time', `${ms}ms`);
+});
+
+router.get('/all', (ctx: Context) => {
+  ctx.response.body = JSON.parse(JSON.stringify(all));
+});
+
+router.get('/belts', (ctx: Context) => {
+  ctx.response.body = JSON.parse(JSON.stringify(belts));
+});
+
+router.get('/buildings', (ctx: Context) => {
+  ctx.response.body = JSON.parse(JSON.stringify(buildings));
+});
+
+router.get('/items', (ctx: Context) => {
+  ctx.response.body = JSON.parse(JSON.stringify(items));
+});
+
+router.get('/miners', (ctx: Context) => {
+  ctx.response.body = JSON.parse(JSON.stringify(miners));
+});
+
+router.get('/resources', (ctx: Context) => {
+  ctx.response.body = JSON.parse(JSON.stringify(resources));
+});
+
+app.addEventListener('listen', ({hostname, port, serverType}) => {
+  console.log(
+    bold('Server: ') + yellow(`${hostname}:${port}`),
+  );
+  console.log(bold('Server Type: ' + yellow(serverType)));
+});
+
+await app.listen({ hostname: '127.0.0.1', port: 8000 });
